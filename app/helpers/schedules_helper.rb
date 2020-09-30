491e29eefd71019f403383825b2ef0f4064152d4
James Dias <jamesdias0@gmail.com>
2020-09-30 15:16:13 -0300

    Added  schedule status badge

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,2 +1,11 @@
 module SchedulesHelper
+  def status_schedule status
+    description = Schedule.human_attribute_name(status)#I18n.t("registration_status.#{status}")
+		case status
+      when Schedule.statuses.keys[0] then "<span class='badge badge-warning'>#{description}</span>".html_safe
+			when Schedule.statuses.keys[1] then "<span class='badge badge-info'>#{description}</span>".html_safe
+			when Schedule.statuses.keys[2] then "<span class='badge badge-success'>#{description}</span>".html_safe
+			when Schedule.statuses.keys[3] then "<span class='badge badge-danger'>#{description}</span>".html_safe
+		end
+	end
 end
